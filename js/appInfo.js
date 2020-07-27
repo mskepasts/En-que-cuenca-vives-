8fc88e8d4e576a87e39bebb43e9b1fcfddf65e89
mskepasts <66769413+mskepasts@users.noreply.github.com>
2020-07-27 15:17:49 -0700

    Add files via upload

--- js/appInfo.js
+++ js/appInfo.js
@@ -1,19 +1,13 @@
-var names = ["Cuenca Tintal", "Humedal Jaun Amerillo", "Quebrada de Fucha", "Quebrada Yomasa", "Rio Fucha", "Rio Salitre", "Rio Tunjuelo", "Torca", "Rio Teusaca"];
-var info = [
-  "in0.",
-  "in1.",
-  "in2.",
-  "in3.",
-  "Cuenca Rio Fucha, es parte montañosa que nace en los cerros orientales y desemboca en el rio Bogota con el nombre Rio Fucha. Es el sector mas antiguo de la ciudad y que tristemente tiene es afectado por contaminación.",
-  "Cuenca Salitre esta ubicado en sector centro-norte del Distrito Capital y las aguas nace en los cerros orientales con el nombre de la quebrada Arzobispo. La cuenca consiste en siete localidades de la división del distrito capital: Santa Fe, Chapinero, Usaquen, Suba, Engativa, Barrios Unidos y Teusaquillo. Cuenca Salitre tiene el parque ecológico distrital del humedal Jaboque, humedal Santa Maria del Lago, humedal Cordoba, humedal Juan Amarillo, y humedal Salitre.",
-  "La cuenca del Rio Tunjuelo contiene aproximadamente 30% de la población de Bogota. Los localidades en esta cuenca son Usme, Ciudad Bolivar, Kennedy, Tunjuelito, Rafeal Uribe, San Critsobal, Puente Aranda, Antonio Narino, Bosa, y Soacha.",
-  "Cuenca Torca, consiste en tres subcuencas principales, El Cedro, San Cristóbal, y Serrezuela. Principales aguas en Torca, es el humedal Torca-Guaymaral, canal Guaymaral y canal Torca. Algunas cosas notables que están ubicado en el área son la biblioteca Julio Mario Santo Domingo, y el parque ecológico distrital de humedal Torca.",
-  "in8."
-]
-var fadeSpeed = 6;
+var fadeSpeed = 8;
 var clicked = false;
 var another = false;
-$('map').imageMapResize();
+
+
+$(document).ready(function() {
+    // $('map').imageMapResize();
+    $('img[usemap]').rwdImageMaps();
+});
+
 $('.map').maphilight({
   fill: true,
   fillColor: '4C4B63',
@@ -36,30 +30,53 @@ $('.map').maphilight({
   shadowPosition: 'outside',
   shadowFrom: false
 });
+$(window).on("resize", function(){
 
+  $('.map').maphilight({
+    fill: true,
+    fillColor: '4C4B63',
+    fillOpacity: 0.5,
+    stroke: true,
+    strokeColor: '4C4B63',
+    strokeOpacity: 1,
+    strokeWidth: 4,
+    fade: true,
+    alwaysOn: false,
+    neverOn: false,
+    groupBy: false,
+    wrapClass: true,
+    shadow: false,
+    shadowX: 0,
+    shadowY: 0,
+    shadowRadius: 6,
+    shadowColor: 'DCA798',
+    shadowOpacity: 0.8,
+    shadowPosition: 'outside',
+    shadowFrom: false
+  });
+})
+// photos\Cuenca Rio Fucha\photo1.png
 function writeInfo(name) {
   $(".front-image").addClass("inBack");
+  $(".round-lrg").attr("src", "circles/" + name + ".png");
+  $(".carouselCircle").attr("src", "circlesForPhotos/" + name + ".png");
+  $("#first").attr("src", "photos/" + name + "/photo1.png");
+  $("#second").attr("src", "photos/" + name + "/photo2.png");
+  $("#third").attr("src", "photos/" + name + "/photo3.png");
   fadeUnfadeTwoElements(".round", ".round-lrg");
-  $("#hiddenPhoto").attr("src", "photos/" + name + ".jpg");
-  $("#hiddenMap").attr("src", "maps/" + name + ".jpg");
-  $(".info-photo").removeClass("hide hiddenPhoto");
-  $("#carousel").removeClass("hide hiddenPhoto");
-  $(".info-map").removeClass("hide hiddenPhoto");
-  // $(".round-lrg").removeClass("hide");
-  $(".info-title").text(names[names.indexOf(name)]);
-  $(".info-info").text(info[names.indexOf(name)]);
 
+  unfade(".second-round-lrg", 2 * fadeSpeed);
+  $(".second-round-lrg").removeClass("hidden")
+  unfade(".carouselCircle", 2 * fadeSpeed);
 
 
 }
 
 function switchSame(name) {
-  if (name != $(".info-title").text()) {
-    fadeUnfadeOneElement(".round-lrg", name);
 
-    $("#hiddenPhoto").attr("src", "photos/" + name + ".jpg");
-    $("#hiddenMap").attr("src", "maps/" + name + ".jpg");
-  }
+  fadeUnfadeOneElement(".round-lrg", name);
+  fadeUnfadeOneElement(".second-round-lrg", name);
+
   // $(".round-lrg").removeClass("hide");
 
 }
@@ -72,8 +89,12 @@ function fadeUnfadeOneElement(element, name) {
     if (op <= .1) {
       clearInterval(timer);
       $(element).removeClass("gone");
-      $(".info-title").text(names[names.indexOf(name)]);
-      $(".info-info").text(info[names.indexOf(name)]);
+      $(".round-lrg").attr("src", "circles/" + name + ".png");
+      $(".carouselCircle").attr("src", "circlesForPhotos/" + name + ".png");
+      $("#first").attr("src", "photos/" + name + "/photo1.png");
+      $("#second").attr("src", "photos/" + name + "/photo2.png");
+      $("#third").attr("src", "photos/" + name + "/photo3.png");
+
 
       unfade(element);
     }
@@ -134,15 +155,32 @@ function fade(element) {
   }, fadeSpeed);
 }
 
+function fade(element, time) {
+  var op = 1; // initial opacity
+  $(element).addClass("gone");
+  var timer = setInterval(function() {
+    if (op <= 0.1) {
+      clearInterval(timer);
+      $(element).hide();
+    }
+
+    $(element).css("opacity", op);
+    $(element).css("filter", 'alpha(opacity=' + op * 100 + ")");
+    op -= op * 0.1;
+  }, time);
+}
+
+
 
 function waitForClick() {
 
   fadeUnfadeTwoElements(".round-lrg", ".round");
   $(".front-image").removeClass("inBack");
-  $(".info-photo").addClass("hide hiddenPhoto");
-  $(".info-map").addClass("hide hiddenPhoto");
+  $(".info-photo").addClass("hide hidden");
+  $(".info-map").addClass("hide hidden");
   $(".round-lrg").addClass("hide");
-  $("#carousel").addClass("hide hiddenPhoto");
+  fade(".second-round-lrg");
+  fade(".carouselCircle");
 
 
 }
@@ -157,6 +195,9 @@ function main() {
   $("area").click(function(event) {
     event.preventDefault()
   });
+  $(window).on("resize", function() {
+    $(".front")
+  })
 
   $(".mapIMG").on("click", function(event) {
     // clicked = false;
